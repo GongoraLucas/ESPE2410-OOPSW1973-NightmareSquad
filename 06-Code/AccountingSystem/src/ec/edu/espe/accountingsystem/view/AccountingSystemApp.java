594826e60e853d7d90d42c9b1e02d4a08ec82054
JAIR SEBASTIAN CHARIJ RAURA <jairsebastian2005@hotmail.com>
2024-11-27 21:59:48 -0500

    Main

---
+++ 06-Code/AccountingSystem/src/ec/edu/espe/accountingsystem/view/AccountingSystemApp.java
@@ -0,0 +1,16 @@
+package ec.edu.espe.accountingsystem.view;
+
+/**
+ *
+ * @author Sebastian Charij
+ */
+
+public class AccountingSystemApp {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Project of Accounting System");
+        System.out.println("Team 2");
+    }
+    
+}
