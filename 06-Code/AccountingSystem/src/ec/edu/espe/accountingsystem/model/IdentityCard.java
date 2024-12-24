9739acc387c9c06b262e82ffe38858db9b9a59ad
JAIR SEBASTIAN CHARIJ RAURA <jairsebastian2005@hotmail.com>
2024-12-23 21:57:15 -0500

    Exception

--- 06-Code/AccountingSystem/src/ec/edu/espe/accountingsystem/model/IdentityCard.java
+++ 06-Code/AccountingSystem/src/ec/edu/espe/accountingsystem/model/IdentityCard.java
@@ -1,5 +1,7 @@
 package ec.edu.espe.accountingsystem.model;
 
+import ec.edu.espe.accountingsystem.exception.InvalidIdentityCardException;
+
 /**
  *
  * @author Sebastian Charij
@@ -8,6 +10,7 @@ package ec.edu.espe.accountingsystem.model;
  * @author David Cuichan
  */
 public class IdentityCard {
+
     private String type;
     private String id;
 
@@ -16,11 +19,30 @@ public class IdentityCard {
         return "IdentityCard{" + "type=" + type + ", id=" + id + '}';
     }
 
-    
-    public IdentityCard(String type, String id) {
+    public IdentityCard() {
+    }
+
+    public IdentityCard(String type, String id) throws InvalidIdentityCardException {
         this.type = type;
+        try {
+            setId(id); // Usar el método setId para validar
+        } catch (InvalidIdentityCardException e) {
+            System.out.println("Error: " + e.getMessage());
+            throw e; // Re-lanzar la excepción si es necesario
+        }
+    }
+
+    public void setId(String id) throws InvalidIdentityCardException {
+        if (!isValidId(id)) {
+            throw new InvalidIdentityCardException("La cédula ingresada no es válida.");
+        }
         this.id = id;
-    }    
+    }
+
+    private boolean isValidId(String id) {
+        // Lógica de validación de cédula (ejemplo simple)
+        return id != null && id.matches("\\d{10}"); // Debe ser un número de 10 dígitos
+    }
 
     /**
      * @return the type
@@ -42,13 +64,4 @@ public class IdentityCard {
     public String getId() {
         return id;
     }
-
-    /**
-     * @param id the id to set
-     */
-    public void setId(String id) {
-        this.id = id;
-    }
-    
-    
 }
