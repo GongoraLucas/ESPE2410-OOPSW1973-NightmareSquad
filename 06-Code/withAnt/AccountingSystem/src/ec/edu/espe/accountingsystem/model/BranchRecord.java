b5ad46bdfffdd06b8b6ca137d7e217f22f226e0a
Cuichan_Hector <hdcuichan@espe.edu.ec>
2025-01-06 10:37:23 -0500

    Inheritance Branch

--- 06-Code/withAnt/AccountingSystem/src/ec/edu/espe/accountingsystem/model/BranchRecord.java
+++ 06-Code/withAnt/AccountingSystem/src/ec/edu/espe/accountingsystem/model/BranchRecord.java
@@ -1,5 +1,3 @@
-
-
 package ec.edu.espe.accountingsystem.model;
 
 /**
@@ -7,61 +5,6 @@ package ec.edu.espe.accountingsystem.model;
  * @author Lucas Gongora
  */
 public class BranchRecord extends Record<Branch>{
-    /*
-    JsonFileManager branches;
-
-    @Override
-    public String toString() {
-       String horizontalLine;
-        StringBuilder content;
-
-        horizontalLine = "-------------------------------------------------------";
-        content = new StringBuilder();
-
-        content.append(horizontalLine);
-        content.append(horizontalLine);
-        content.append(String.format("\n| %12s | %12s | %30s | %12s | %12s |\n",
-                "ID", "Name", "Address", "Phone number", "Manager"));
-
-        for (Branch branch : branches.read(Branch.class)) {
-            content.append(branch.toString());
-        }
-
-        content.append(horizontalLine);
-        content.append("\n");
-
-        return content.toString();
-    }
-    
-    
-    public BranchRecord(String branchesFile) {
-        this.branches = new JsonFileManager(branchesFile);
-    }
-    
-    public void add(Branch branch){
-        this.branches.add(branch, Branch.class);
-    }
-    
-    public void delete(String branchId){
-        this.branches.delete(branchId, Branch.class);
-    }
-    
-    public void update(String branchId,Branch branch){
-        this.branches.update(branchId, branchId, Branch.class);
-    }
-    
-    public Branch findBranchById(String branchId) {
-        for (Branch branch : this.branches.read(Branch.class)) {
-            if (branch.getId().equals(branchId)) {
-                return branch;
-            }
-        }
-        return null;
-    }
-    public ArrayList<Branch> getBranches(){
-        return this.branches.read(Branch.class);
-    }
-    */
     public BranchRecord(String filename) {
         super(filename);
     }
@@ -95,5 +38,5 @@ public class BranchRecord extends Record<Branch>{
                .append("\n");
         
         return content.toString();
-    }
+    }    
 }
